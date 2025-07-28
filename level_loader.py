6002956e2f7bc300b4f610b86ad559385231bdc9
arsandrej <andrej.arsovski.1@students.finki.ukim.mk>
2025-07-28 11:16:43 +0200

    Added a short end of level display of the finished state

--- level_loader.py
+++ level_loader.py
@@ -2,3 +2,12 @@ def load_level(file_path):
     with open(file_path) as f:
         lines = [line.rstrip('\n') for line in f]
     return lines
+
+def apply_solution(state, solution):
+    current_state = state
+    for move in solution:
+        for direction, new_state in current_state.get_successors():
+            if direction == move:
+                current_state = new_state
+                break
+    return current_state
--- main.py
+++ main.py
@@ -3,26 +3,38 @@ from solvers.astar_solver import astar_solver
 from solvers.bfs_solver import bfs_solver
 from solvers.dfs_solver import dfs_solver
 from level_loader import load_level
+from level_loader import apply_solution
 
-
-level = load_level("levels/frozen_test.txt")
-
+level = load_level("levels/level2.txt")
 initial_state = SokobanState(level)
 
 solution = astar_solver(initial_state)
 if solution:
     print("A* Solution:", solution)
+    final_state = apply_solution(initial_state, solution)
+    print("Final A* State:")
+    print(final_state)
 else:
     print("No A* solution found.")
+
+print()
 
 solutionbfs = bfs_solver(initial_state)
 if solutionbfs:
     print("BFS Solution:", solutionbfs)
+    final_state_bfs = apply_solution(initial_state, solutionbfs)
+    print("Final BFS State:")
+    print(final_state_bfs)
 else:
     print("No BFS solution found.")
+
+print()
 
 solutiondfs = dfs_solver(initial_state)
 if solutiondfs:
     print("DFS Solution:", solutiondfs)
+    final_state_dfs = apply_solution(initial_state, solutiondfs)
+    print("Final DFS State:")
+    print(final_state_dfs)
 else:
     print("No DFS solution found.")
