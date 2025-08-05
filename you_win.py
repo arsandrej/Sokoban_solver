17fe95f399a138419525a580f996097a3c08aea0
arsandrej <andrej.arsovski.1@students.finki.ukim.mk>
2025-08-05 12:45:43 +0200

    Added a "You win!" screen

---
+++ you_win.py
@@ -0,0 +1,31 @@
+import pygame
+from level_loader import load_images
+
+_images = load_images()
+_player_img = _images["player"]
+_images = load_images()
+player_img_scaled = pygame.transform.scale(_images["player"], (128, 128))
+
+def you_win(screen, width, height):
+    font_big = pygame.font.SysFont(None, 72)
+    font_small = pygame.font.SysFont(None, 32)
+
+    text = font_big.render("You Win!", True, (255, 255, 0))
+    text_rect = text.get_rect(center=(width // 2, height // 2 - 30))
+
+    padding = 10
+    player_rect_1 = player_img_scaled.get_rect()
+    player_rect_1.midleft = (text_rect.right + padding, text_rect.centery)
+
+    player_rect_2 = player_img_scaled.get_rect()
+    player_rect_2.midright= (text_rect.left - padding, text_rect.centery)
+
+    instruction = font_small.render("Press R to reset or ESC to quit", True, (200, 200, 200))
+    instruction_rect = instruction.get_rect(center=(width // 2, height // 2 + 40))
+
+    screen.fill((0, 0, 0))
+    screen.blit(text, text_rect)
+    screen.blit(player_img_scaled, player_rect_1)
+    screen.blit(player_img_scaled, player_rect_2)
+    screen.blit(instruction, instruction_rect)
+    pygame.display.flip()
