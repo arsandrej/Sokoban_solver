cc573599ad5e04f6a5a81eb4417980a3d77209a6
arsandrej <andrej.arsovski.1@students.finki.ukim.mk>
2025-08-07 09:59:43 +0200

    Added full game loop with Main menu return potential

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from visualize import run_game
 from menu import run_menu
 from solo_game import run_solo_game
 
-level = load_level("levels/level13.txt")
+level = load_level("levels/level1.txt")
 
 choice, path = run_menu()
 if choice is None:
@@ -18,18 +18,37 @@ if path is not None:
     level = load_level(path)
 
 initial_state = SokobanState(level)
-initial_state.print_dead_squares()
+# initial_state.print_dead_squares()
 
-if choice == "solo":
-    run_solo_game(initial_state, astar_solver)
-elif choice == "ai":
-    astar_solution, astar_stats = run_solver("A*", astar_solver, initial_state)
-    bfs_solution, bfs_stats = run_solver("BFS", bfs_solver, initial_state)
-    dfs_solution, dfs_stats = run_solver("DFS", dfs_solver, initial_state)
-    run_game(initial_state, astar_solution, astar_stats, bfs_solution, bfs_stats, dfs_solution, dfs_stats)
+while True:
+    if choice == "solo":
+        result = run_solo_game(initial_state, astar_solver)
+        if result is None:
+            break
+        choice, path = run_menu()
+        if path is not None:
+            level = load_level(path)
+        initial_state = SokobanState(level)
 
-elif choice == "settings":
-    run_solo_game(initial_state, astar_solver)
+    elif choice == "ai":
+        astar_solution, astar_stats = run_solver("A*", astar_solver, initial_state)
+        bfs_solution, bfs_stats = run_solver("BFS", bfs_solver, initial_state)
+        dfs_solution, dfs_stats = run_solver("DFS", dfs_solver, initial_state)
+        result = run_game(initial_state, astar_solution, astar_stats, bfs_solution, bfs_stats, dfs_solution, dfs_stats)
+        if result is None:
+            break
+        choice, path = run_menu()
+        if path is not None:
+            level = load_level(path)
+        initial_state = SokobanState(level)
+
+
+    elif choice == "settings":
+        choice, path = run_menu()
+        if path is not None:
+            level = load_level(path)
+        initial_state = SokobanState(level)
+
 
 
 
